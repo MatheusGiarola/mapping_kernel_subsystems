e6569bee3ed5aa270e720db697c69c6f4ba4858d
Matheus Giarola <97200978+MatheusGiarola@users.noreply.github.com>
2026-09-20 16:52:01 -0300

    mapping_subs: paralellized script
    Changed original while loop by a
    GNU parallel call in order to optimise code
    and reduce processing time.
    
    Furthermore, the table is being written in
    the base dir directily, preventing from
    missing the csv file during executions.

--- mapping_subs.sh
+++ mapping_subs.sh
@@ -3,24 +3,40 @@
 #environment variables
 BASE_DIR="$HOME/Documentos/ic/mapping_kernel_subsystems"
 KERNEL_DIR="$BASE_DIR/linux/" #path to kernel files
-GET_MAINTAINER="$KERNEL_DIR/scripts/get_maintainer.pl"
-FLAGS=(--nogit --nogit-fallback --nom --nor --separator=";" )
-CSV_FILE="resultados.csv"
+CSV_FILE="$BASE_DIR/resultados.csv"
+export GET_MAINTAINER="$KERNEL_DIR/scripts/get_maintainer.pl"
 
 cd "$KERNEL_DIR"
 echo "Diretorio,Arquivo,Subsistemas,Listas de Email" > "$CSV_FILE"
 
-#get each file information and update the table
-git ls-files -z drivers/iio | while IFS= read -r -d "" line
-do
+#run the get maintainer script in a received file
+process_line () {
+    line="$1"
 
     dir=$(dirname "$line")
     nome=$(basename "$line")
-    subsistemas=$(perl "$GET_MAINTAINER" "${FLAGS[@]}" --noemail --subsystem --nol -f "$line")
-    mailing_list=$(perl "$GET_MAINTAINER" "${FLAGS[@]}" -f "$line")
-    echo "\"$dir\",\"$nome\",\"$subsistemas\",\"$mailing_list\"" >> "$CSV_FILE"
+    subsistemas=$(perl "$GET_MAINTAINER" \
+                        --nogit \
+                        --nogit-fallback \
+                        --nom \
+                        --nor \
+                        --separator=";" \
+                        --noemail    \
+                        --subsystem \
+                        --nol \
+                        -f "$line")
 
-done
+    mailing_list=$(perl "$GET_MAINTAINER" \
+                        --nogit \
+                        --nogit-fallback \
+                        --nom \
+                        --nor  \
+                        --separator=";" \
+                        -f "$line")
 
+    echo "\"$dir\",\"$nome\",\"$subsistemas\",\"$mailing_list\""
+}
+export -f process_line
 
-mv "$CSV_FILE" "$BASE_DIR"
+#Process all the files in parallel and update the table
+git ls-files -z drivers | parallel -0 process_line >> "$CSV_FILE"
